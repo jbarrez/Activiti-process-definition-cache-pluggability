ab5ca9f1067ffab278583c1d940870659c357305
Joram Barrez <joram.barrez@gmail.com>
2012-12-20 19:24:03 +0100

    Tweaking formatting

--- src/main/java/org/activiti/RunProcessDefinitionCacheDemo.java
+++ src/main/java/org/activiti/RunProcessDefinitionCacheDemo.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
-import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.impl.util.LogUtil;
 import org.h2.tools.Server;
 
@@ -20,72 +19,74 @@ import org.h2.tools.Server;
  */
 public class RunProcessDefinitionCacheDemo {
 
-  private static final Logger logger = Logger.getLogger(RunProcessDefinitionCacheDemo.class.getName());
+    private static final Logger logger = Logger.getLogger(RunProcessDefinitionCacheDemo.class.getName());
 
-  static {
-    LogUtil.readJavaUtilLoggingConfigFromClasspath();
-  }
+    static {
+        LogUtil.readJavaUtilLoggingConfigFromClasspath();
+    }
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    boolean runDefault = validateArguments(args);
-    boolean distributed = !runDefault;
-    
-    if(distributed) {
-      // Infinispan spits out A LOT logging.
-      // So in the distributed case, we set it a bit higher
-      Logger rootLogger = Logger.getLogger("");
-      rootLogger.setLevel(Level.INFO);
-    }
-    
-    // Setup database
-    Server h2Server = startH2Server();
-    
-    if (!areDemoProcessesDeployed()) { // extra cluster nodes beyond the first one should not deploy new processes
-      // Deploy test processes
-      int numberOfProcessDefinitions = Integer.valueOf(askForInput("How many process definitions do you want?"));
-      deployProcessDefinitions(numberOfProcessDefinitions);
-    }
-    
-    // Create Process Engine based
-    ProcessEngine processEngine = null;
-    if (runDefault) {
-      processEngine = ProcessEngines.getDefaultProcessEngine();
-    } else {
-      ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
-              .createProcessEngineConfigurationFromResource("activiti-with-distributed-deployment-cache.cfg.xml");
-      processEngine = processEngineConfiguration.buildProcessEngine();
-    }
-    
-    // Start instances
-    int numberOfInstances = Integer.valueOf(askForInput("How many process instance do I need to start?"));
-    startProcessInstances(processEngine, numberOfInstances);
-    
-    System.gc();
-    askForInput("Type in anything to stop the demo");
+        boolean runDefault = validateArguments(args);
+        boolean distributed = !runDefault;
 
-    // Shutdown
-    if (h2Server != null) {
-      h2Server.stop();
-    }
-    logger.info("All Done!");
-  }
+        if (distributed) {
+            // Infinispan spits out A LOT logging.
+            // So in the distributed case, we set it a bit higher
+            Logger rootLogger = Logger.getLogger("");
+            rootLogger.setLevel(Level.INFO);
+        }
 
-  private static boolean validateArguments(String[] args) throws Exception {
-    if (args.length == 0) {
-      System.err.println("I can't guess which configuration you want to run. You should provide 'default' or 'distributed' as argument");
-      throw new RuntimeException();
+        // Setup database
+        Server h2Server = startH2Server();
+
+        if (!areDemoProcessesDeployed()) { // extra cluster nodes to the same database beyond the first one should not deploy new processes
+            // Deploy test processes
+            int numberOfProcessDefinitions = Integer.valueOf(askForInput("How many process definitions do you want?"));
+            deployProcessDefinitions(numberOfProcessDefinitions);
+        }
+
+        // Create Process Engine based
+        ProcessEngineConfiguration processEngineConfiguration = null;
+        if (runDefault) {
+            processEngineConfiguration = ProcessEngineConfiguration
+                    .createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+        } else {
+            processEngineConfiguration = ProcessEngineConfiguration
+                    .createProcessEngineConfigurationFromResource("activiti-with-distributed-deployment-cache.cfg.xml");
+        }
+        ProcessEngine processEngine = processEngineConfiguration.buildProcessEngine();
+
+        // Start process instances
+        int numberOfInstances = Integer.valueOf(askForInput("How many process instance do I need to start?"));
+        startProcessInstances(processEngine, numberOfInstances);
+
+        // We hint the jvm to do a garbage collection, so we can see in a profiler exactly how much memory is used
+        System.gc();
+        askForInput("Type in anything to stop the demo");
+
+        // Shutdown H2 db server
+        if (h2Server != null) {
+            h2Server.stop();
+        }
+        logger.info("All Done!");
     }
-    
-    if (args[0].toLowerCase().equals("default")) {
-      return true;
-    } else if (args[0].toLowerCase().equals("distributed")) {
-      return false;
-    } else {
-      System.err.println("I tried hard, really! But I cant decipher which configuration you need. Use 'default' or 'distributed'.");
-      throw new RuntimeException();
+
+    private static boolean validateArguments(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.err.println("I can't guess which configuration you want to run. You should provide 'default' or 'distributed' as argument");
+            throw new RuntimeException();
+        }
+
+        if (args[0].toLowerCase().equals("default")) {
+            return true;
+        } else if (args[0].toLowerCase().equals("distributed")) {
+            return false;
+        } else {
+            System.err.println("I tried hard, really! But I cant decipher which configuration you need. Use 'default' or 'distributed'.");
+            throw new RuntimeException();
+        }
     }
-  }
-  
+
 
 }
